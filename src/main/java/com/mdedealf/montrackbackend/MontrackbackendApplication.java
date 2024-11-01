049a9dad933b8e9b3d8523ac4b3604aee5377c52
mdedealf <dede.alfaruqq@gmail.com>
2024-11-01 13:31:51 +0700

    initial: initial setup spring boot

---
+++ src/main/java/com/mdedealf/montrackbackend/MontrackbackendApplication.java
@@ -0,0 +1,13 @@
+package com.mdedealf.montrackbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MontrackbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MontrackbackendApplication.class, args);
+	}
+
+}
