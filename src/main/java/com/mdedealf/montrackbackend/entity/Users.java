44ce51c98a4355668343efa115bc92d5646ba8b0
mdedealf <dede.alfaruqq@gmail.com>
2024-11-02 19:55:41 +0700

    refactor: data source config using env

---
+++ src/main/java/com/mdedealf/montrackbackend/entity/Users.java
@@ -0,0 +1,12 @@
+package com.mdedealf.montrackbackend.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Users {
+    private int userId;
+}
