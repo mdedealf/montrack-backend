480a4ba494f15e265f1376cdc74cf1699d382158
mdedealf <dede.alfaruqq@gmail.com>
2024-11-03 16:40:32 +0700

    feat(pocket): adding pocket entity

--- src/main/java/com/mdedealf/montrackbackend/entity/Pocket.java
+++ src/main/java/com/mdedealf/montrackbackend/entity/Pocket.java
@@ -4,9 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.ZonedDateTime;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Pocket {
     private Long pocketId;
+    private Long walletId;
+    private String name;
+    private String description;
+    private String emojiCode;
+    private Double totalAmount;
+    private Double currentAmount;
+    private ZonedDateTime createdAt;
+    private ZonedDateTime updatedAt;
+    private ZonedDateTime deletedAt;
 }
