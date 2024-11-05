23b2a5d44bca0757b28d2e81e2e722c787078b45
mdedealf <dede.alfaruqq@gmail.com>
2024-11-05 10:58:29 +0700

    feat: add moneyRecords JPA entity

---
+++ src/main/java/com/mdedealf/montrackbackend/entity/MoneyRecords.java
@@ -0,0 +1,86 @@
+package com.mdedealf.montrackbackend.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.math.BigDecimal;
+import java.time.ZonedDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "money_records", schema = "montrack")
+public class MoneyRecords {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "money_records_id_gen")
+    @SequenceGenerator(name = "money_records_id_gen", sequenceName = "money_records_money_record_id_seq", allocationSize = 1)
+    @Column(name = "money_record_id", nullable = false)
+    private Long moneyRecordId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pocket_id")
+    private Pockets pocket;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "goal_id")
+    private Goals goal;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "wallet_id")
+    private Wallets wallet;
+
+    @NotNull
+    @Size(max = 150)
+    @Column(name = "name", nullable = false, length = 150)
+    private String name;
+
+    @NotNull
+    @Size(max = 50)
+    @Column(name = "type", nullable = false, length = 50)
+    private String type;
+
+    @NotNull
+    @Column(name = "transaction_amount", nullable = false)
+    private BigDecimal transactionAmount;
+
+    @Column(name = "transaction_date")
+    private ZonedDateTime transactionDate;
+
+    @Size(max = 150)
+    @Column(name = "description", length = 150)
+    private String description;
+
+    @Size(max = 255)
+    @Column(name = "file_attachment", length = 255)
+    private String fileAttachment;
+
+    @NotNull
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "created_at", nullable = false)
+    private ZonedDateTime createdAt;
+
+    @NotNull
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "updated_at", nullable = false)
+    private ZonedDateTime updatedAt;
+
+    @Column(name = "created_at")
+    private ZonedDateTime deletedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = ZonedDateTime.now();
+        this.updatedAt = ZonedDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = ZonedDateTime.now();
+    }
+}
